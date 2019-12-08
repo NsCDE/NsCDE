cd341403caf64c14228e84ab1e3fee1b65245cc2
Hegel3DReloaded <nscde@protonmail.com>
2019-12-09 00:39:58 +0100

    Backdrops update

--- NsCDE/share/backdrops/Spagetti.pm
+++ NsCDE/share/backdrops/Spagetti.pm
@@ -2,9 +2,9 @@
 /* This file is a part of the NsCDE - Not so Common Desktop Environment */
 static char * Spagetti_pm[] = {
 "128 128 3 1",
-"    s bottomShadowColor   m black   c #636363636363",
-".   s background   m black   c #949494949494",
-"+   s topShadowColor   m white   c #bdbdbdbdbdbd",
+"+   s bottomShadowColor   m black   c #636363636363",
+"    s background   m black   c #949494949494",
+".   s selectColor   m white   c #bdbdbdbdbdbd",
 "+. ++. +. +. +.  . ++. .+ ..++++++ .++  .+.+ ...+ .....+ +. .+. +.  ..+   +. +.......+ .+ ..+ . ..+++++...+ ..+   .+. .+ ..+.  .",
 ". .+  +.+.+ .+ ...+. +.+. ..+      +.+ .+. + ..+. ..+++++. ..+ .. ..++. +.+ .+ ...+.++ .+ .+. ...++    +.+. .+.++..+ ...+++++..+",
 " ..+ +. .++ .+ ...+ ..+. ...+ .....+ + .+ .+ ...+..+.    +...+ ..+++.  +.+.++.++.+.+.  .+ ++ .+++.+ ..+. + ..+ . ++.+.... +  ++.",
