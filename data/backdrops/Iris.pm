14e79410fd11437252b14d6156ecb8b5c1f0b25a
Hegel3DReloaded <nscde@protonmail.com>
2021-11-13 16:45:19 +0100

    Evoid using copyrighted names for art pieces

--- data/backdrops/Iris.pm
+++ data/backdrops/Iris.pm
@@ -1,5 +1,5 @@
 /* XPM */
-static char * Irix_pm[] = {
+static char * Iris_pm[] = {
 "128 128 2 1",
 "+      s bottomShadowColor   m black   c #636363636363",
 ".      s background   m black c #737373737373",
